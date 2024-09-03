43e3f4363e143af5eef8d4bef7dd1462d3c27536
ElijahBelov <mewoak@gmail.com>
2024-09-03 17:05:59 -0400

    mess

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,75 @@
+import { createSlice } from '@reduxjs/toolkit';
 
-const CartSlice = ({
- 
+const initialState = {
+    cartItems: [],
+};
+
+const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
+
+
+const CartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addItemToCart(state, action) {
+            setDisabledProducts([...disabledProducts, action.payload.id]); // Mark the product as disabled
+            let existingItemID = action.payload.id;
+            for (let i = 0; i < state.cartItems.length; i++) {
+                if (state.cartItems[i].id === existingItemID){
+                    state.cartItems[i].quantity += 1;
+                    return;
+                }
+            }
+            state.cartItems.push({...action.payload, quantity: 1});
+        },
+
+        removeItemFromCart(state, action) {
+            for (let i = 0; i < state.cartItems.length; i++) {
+                if (state.cartItems[i].id === action.payload){
+                    state.cartItems.splice(i, 1);
+                    return;
+                }
+            }
+        },
+
+        clearCart(state) {
+            state.cartItems = [];
+            setDisabledProducts([]);
+            
+        },
+
+        increaseItemQuantity(state, action){
+            let existingItemID = action.payload;
+            for (let i = 0; i < state.cartItems.length; i++) {
+                if (state.cartItems[i].id === existingItemID){
+                    state.cartItems[i].quantity += 1;
+                    return;
+                }
+            }
+        },
+
+        decreaseItemQuantity(state, action){
+            let existingItemID = action.payload;
+            for (let i = 0; i < state.cartItems.length; i++) {
+                if (state.cartItems[i].id === existingItemID && state.cartItems[i].quantity > 0){
+                    state.cartItems[i].quantity -= 1;
+                    if (state.cartItems[i].quantity === 0) {
+                        setDisabledProducts(disabledProducts.toSpliced(i, 1));
+                    }
+                    return;
+                }
+            }
+        },
+    }
 });
+
+export const {
+  addItemToCart,
+  removeItemFromCart,
+  clearCart,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+} = CartSlice.actions;
+export default CartSlice.reducer;
 
 
